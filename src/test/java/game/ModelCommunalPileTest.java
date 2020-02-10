d2a1211e4ab0c438f1af427c7262d578ccb11f7a
Graham Darroch <0502755d@student.gla.ac.uk>
2020-02-10 17:10:05 +0000

    ModelPlayer and ModelCommunalPile tests

--- src/test/java/game/ModelCommunalPileTest.java
+++ src/test/java/game/ModelCommunalPileTest.java
@@ -2,13 +2,59 @@ package game;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ModelCommunalPileTest {
+	ModelCommunalPile pile = null;
+	String[] attributeList = null;
+	String[] info = null;
+	ModelCard card = null;
+	ModelPlayer player = null;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@BeforeEach
+	public void setUp(){
+		this.pile = new ModelCommunalPile();
+		this.attributeList = new String[3];
+		this.attributeList[0] = "name";
+		this.attributeList[1] = "speed";
+		this.attributeList[2] = "thrust";
+		this.info = new String[3];
+		this.info[0] = "shipName";
+		this.info[1] = "0";
+		this.info[2] = "1";
+		this.card = new ModelCard(this.info, this.attributeList);
+		this.player = new ModelPlayer("Henrietta");
 	}
 
+	@Test
+	public void addToHandTest() {
+		this.pile.add(this.card);
+		assertEquals(this.card,this.pile.get(0));
+	}
+
+	@Test
+	public void giveToPlayerOneCardTest(){
+		this.pile.add(this.card);
+		this.pile.pickedUpByWinner(this.player);
+		assertTrue(this.pile.size() == 0);
+		assertEquals(this.card,this.player.getActiveCard());
+	}
+
+	@Test
+	public void giveToPlayerThreeCardsTest(){
+		this.pile.add(this.card);
+		this.pile.pickedUpByWinner(this.player);
+		assertFalse(true);
+	}
+
+	@AfterEach
+	public void tearDown(){
+		this.pile = null;
+		this.attributeList = null;
+		this.info = null;
+		this.card = null;
+		this.player = null;
+	}
 }
--- src/test/java/game/ModelPlayerTest.java
+++ src/test/java/game/ModelPlayerTest.java
@@ -2,13 +2,110 @@ package game;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ModelPlayerTest {
+public class ModelPlayerTest {	
+	ModelPlayer player = null;
+	String name = null;
+	String[] attributeList = null;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public ModelCard newCard(String[] cardValues){
+		ModelCard card = new ModelCard(cardValues, this.attributeList);
+		return card;
 	}
 
+	public String[] newCardValuesArray(String...s){
+		String[] cardValues = new String[s.length];
+		for(int i = 0 ; i < s.length; ++i){
+			cardValues[i] = s[i];
+		}
+		return cardValues;
+	}
+
+	@BeforeEach
+	public void setUp(){
+		this.name = "Henrietta";
+		this.player = new ModelPlayer(this.name);
+		this.attributeList = new String[3];
+		this.attributeList[0] = "name";
+		this.attributeList[1] = "speed";
+		this.attributeList[2] = "thrust";
+	}
+
+	@Test
+	public void getNameTest(){
+		assertEquals(this.name, this.player.getName());
+	}
+
+	@Test
+	public void initialHandEmptyTest(){
+		assertTrue(this.player.isHandEmpty());
+	}
+
+	public void getActiveCardTest(){
+		String[] cardValues = newCardValuesArray("0","1","2");
+		ModelCard card = newCard(cardValues);
+		this.player.addToHand(card);
+		assertEquals(card,this.player.getActiveCard());
+	}
+
+	/*
+	 * Cards are inserted at the beginning of the hand array.
+	 * Active card is the last element of the hand array
+	*/
+	@Test
+	public void getActiveCardHandSize2Test(){
+		String[] cardValues = newCardValuesArray("0","1","2");		
+		ModelCard card1 = newCard(cardValues);
+		this.player.addToHand(card1);
+		cardValues = newCardValuesArray("3","0","2");
+		ModelCard card2 = newCard(cardValues);
+		this.player.addToHand(card2);
+		assertEquals(card1,this.player.getActiveCard());
+	}
+
+	@Test
+	public void removeOnlyCardTest(){
+		String[] cardValues = newCardValuesArray("0","1","2");		
+		ModelCard card1 = newCard(cardValues);
+		this.player.addToHand(card1);
+		this.player.removeFromHand(card1);
+		assertTrue(this.player.isHandEmpty());
+	}
+
+	@Test
+	public void remove1Of2CardsTest(){
+		String[] cardValues = newCardValuesArray("0","1","2");		
+		ModelCard card1 = newCard(cardValues);
+		this.player.addToHand(card1);
+		cardValues = newCardValuesArray("3","0","2");
+		ModelCard card2 = newCard(cardValues);
+		this.player.addToHand(card2);
+		this.player.removeFromHand(card1);
+		ArrayList<ModelCard> cards = this.player.getHand();
+		assertTrue(cards.size() == 1);
+		assertEquals(card2,cards.get(0));	
+	}
+
+	@Test
+	public void toStringTest(){
+		String[] cardValues = newCardValuesArray("0","1","2");		
+		ModelCard card1 = newCard(cardValues);
+		this.player.addToHand(card1);
+		String expected = this.player.getName();
+		expected += "\n";
+		expected += card1.toString();
+		assertEquals(expected,this.player.toString());
+	}
+
+	@AfterEach
+	public void tearDown(){
+		this.player = null;
+		this.name = null;
+		this.attributeList = null;
+	}
 }
